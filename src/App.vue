db9f3718acd02897f38c07d7a9be516527e63f31
Dante Martinez <mzadantemartinez@gmail.com>
2024-08-10 21:36:56 -0300

    capitulo 2

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,13 @@
 <script setup>
-
-//puro js
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+const nombreApellido= 'Juan Perez';
 </script>
 
 <template>
-  <!--html-->
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+ <header>
+  <h1> {{ nombreApellido }} </h1>
+  <p> Bienvenido a Vue 3 </p>
+ </header>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
