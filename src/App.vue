dfc8a51060f8dc46be35ee875bfdc05aa086a7fc
Dante Martinez <mzadantemartinez@gmail.com>
2024-08-12 15:14:22 -0300

    Capitulo 3 Barra de navegacion

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup>
-const nombreApellido= 'Juan Perez';
+import NavBar from './components/NavBar.vue';
 </script>
 
 <template>
- <header>
-  <h1> {{ nombreApellido }} </h1>
-  <p> Bienvenido a Vue 3 </p>
- </header>
+    <header>
+        <!--Barra de Navegacion-->
+        <NavBar />
+            
+    </header>
+    <main>
+        <section id="educacion"><h2>Educación - Cursos</h2></section>
+        <section id="experiencia"><h2>Experiencia</h2></section>
+        <section id="proyectos"><h2>Proyectos</h2></section>
+        <section id="habilidades"><h2>Habilidades</h2></section>
+        <section id="intereses"><h2>Intereses</h2></section>
+    </main>
 </template>
 
 <style scoped>
+
 </style>
