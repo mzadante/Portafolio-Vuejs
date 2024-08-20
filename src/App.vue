2ff2dd382a90318c633d18f51f235631149a4ab9
Dante Martinez <mzadantemartinez@gmail.com>
2024-08-20 19:52:23 -0300

    update capitulo 5

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
+import Educacion from './components/Educacion.vue';
 </script>
 
 <template>
@@ -11,7 +12,9 @@ import DatosPersonales from './components/DatosPersonales.vue';
         <DatosPersonales />
     </header>
     <main>
-        <section id="educacion"><h2>Educación - Cursos</h2></section>
+        <section id="educacion"><h2>Educación - Cursos</h2>
+        <Educacion />
+        </section>
         <section id="experiencia"><h2>Experiencia</h2></section>
         <section id="proyectos"><h2>Proyectos</h2></section>
         <section id="habilidades"><h2>Habilidades</h2></section>
