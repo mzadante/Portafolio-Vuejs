c55879203c7c3072cf1909f52c78e91e1f0452c2
Dante Martinez <mzadantemartinez@gmail.com>
2024-09-03 09:39:49 -0300

    comenzando cap 7 proyectos galeria

--- src/App.vue
+++ src/App.vue
@@ -9,23 +9,29 @@ import ExperienciaComponente from './components/ExperienciaComponente.vue';
     <header>
         <!--Barra de Navegacion-->
         <NavBar />
-        <!--Componente Datos Personales-->    
+        <!--Componente Datos Personales-->
         <DatosPersonales />
     </header>
     <main>
         <!--el estilo css de section se encuentra en base.css-->
-        <section id="educacion"><h2>Educación - Cursos</h2>
-        <Educacion />
+        <section id="educacion">
+            <h2>Educación - Cursos</h2>
+            <Educacion />
         </section>
-        <section id="experiencia"><h2>Experiencia</h2>
-        <ExperienciaComponente />
+        <section id="experiencia">
+            <h2>Experiencia</h2>
+            <ExperienciaComponente />
         </section>
-        <section id="proyectos"><h2>Proyectos</h2></section>
-        <section id="habilidades"><h2>Habilidades</h2></section>
-        <section id="intereses"><h2>Intereses</h2></section>
+        <section id="proyectos">
+            <h2>Proyectos</h2>
+        </section>
+        <section id="habilidades">
+            <h2>Habilidades</h2>
+        </section>
+        <section id="intereses">
+            <h2>Intereses</h2>
+        </section>
     </main>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
