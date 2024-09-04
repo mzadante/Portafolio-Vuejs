834009d07fa5d308c93eb0f5dcfc3c8c758d5e67
Dante Martinez <mzadantemartinez@gmail.com>
2024-09-04 08:53:31 -0300

    capitulo 7 falta js

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@ import NavBar from './components/NavBar.vue';
 import DatosPersonales from './components/DatosPersonales.vue';
 import Educacion from './components/EducacionComponente.vue';
 import ExperienciaComponente from './components/ExperienciaComponente.vue';
+import ProyectosComponente from './components/ProyectosComponente.vue';
 </script>
 
 <template>
@@ -16,14 +17,18 @@ import ExperienciaComponente from './components/ExperienciaComponente.vue';
         <!--el estilo css de section se encuentra en base.css-->
         <section id="educacion">
             <h2>Educación - Cursos</h2>
+            <!--Componente Educación-->
             <Educacion />
         </section>
         <section id="experiencia">
             <h2>Experiencia</h2>
+            <!--Componente Experiencia-->
             <ExperienciaComponente />
         </section>
         <section id="proyectos">
             <h2>Proyectos</h2>
+            <!--Componente Proyectos-->
+            <ProyectosComponente />
         </section>
         <section id="habilidades">
             <h2>Habilidades</h2>
